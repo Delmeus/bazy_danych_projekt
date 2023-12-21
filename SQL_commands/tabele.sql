d66116ddce4e36180a5109b29240a206b8ca53e1
Delmeus <93483813+Delmeus@users.noreply.github.com>
2023-12-21 13:41:28 +0100

    Tables, views, indices

---
+++ SQL_commands/tabele.sql
@@ -0,0 +1,69 @@
+--
+-- Tworzenie tabel
+--
+
+CREATE TABLE clients (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    address VARCHAR(90) NOT NULL,
+    city VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE accounts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    account_number VARCHAR(26) NOT NULL UNIQUE,
+    balance DECIMAL(15, 2) NULL,
+    client_id INT UNIQUE,
+    CONSTRAINT fk_client_id FOREIGN KEY (client_id) REFERENCES clients(id)
+);
+
+CREATE TABLE departments (
+    id INT PRIMARY KEY ,
+    department_name VARCHAR(30) NOT NULL,
+    address VARCHAR(90) NOT NULL
+);
+
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    position VARCHAR(50) NULL,
+    department_id INT NULL,
+    FOREIGN KEY  (department_id) REFERENCES departments(id)
+);
+
+CREATE TABLE transaction_type (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+	transaction_name VARCHAR(60) NULL
+);
+
+CREATE TABLE transactions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    amount DECIMAL(15, 2) NULL , 
+    type_id INT NULL,
+    account_id INT NOT NULL,
+    FOREIGN KEY (type_id) REFERENCES transaction_type(id),
+    FOREIGN KEY (account_id) REFERENCES accounts(id)
+);
+
+CREATE TABLE Credit_Card (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    card_number VARCHAR(16),
+    expiry_date DATE NOT NULL,
+    client_id INT NOT NULL,
+    producent_name VARCHAR(30),
+    FOREIGN KEY (client_id) REFERENCES Clients(id)
+);
+
+--
+-- Usuwanei tabel
+--
+
+DROP TABLE credit_card;
+DROP TABLE transactions;
+DROP TABLE transaction_type;
+DROP TABLE employees;
+DROP TABLE departments;
+DROP TABLE accounts;
+DROP TABLE clients;
---
+++ SQL_commands/widoki.sql
@@ -0,0 +1,137 @@
+--
+-- Widok do sprawdzania stanu konta
+--
+
+CREATE VIEW check_balance_view AS 
+SELECT
+	CONCAT(clients.first_name, ' ', clients.last_name) AS "Imie i nazwisko",
+	accounts.account_number AS "Numer konta", 
+    accounts.balance AS "Saldo"
+FROM 
+	accounts
+JOIN 
+	clients ON clients.id = accounts.client_id
+GROUP BY
+	clients.id, "Imie i nazwisko", accounts.account_number;
+    
+SELECT * FROM check_balance_view;
+
+--
+-- Widok do sprawdzania ilosci kart
+--
+
+CREATE VIEW client_cards_view AS
+SELECT
+    CONCAT(clients.first_name, ' ', clients.last_name) AS "Imie i nazwisko",
+    COUNT(credit_card.client_id) AS "Ilosc kart"
+FROM
+    clients
+JOIN
+    credit_card ON clients.id = credit_card.client_id
+GROUP BY
+    clients.id, "Imie i nazwisko";
+
+
+SELECT * FROM client_cards_view;
+
+--
+-- Widok do sprawdzenia informacji o klientach
+--
+
+CREATE VIEW client_credentials_view AS
+SELECT
+    CONCAT(clients.first_name, ' ', clients.last_name) AS "Imie i nazwisko",
+    CONCAT(clients.address, ' ', clients.city) AS "Adres zamieszkania",
+    GROUP_CONCAT(accounts.account_number) AS "Numer konta"
+FROM
+    clients
+JOIN
+    accounts ON clients.id = accounts.client_id
+GROUP BY
+    clients.id, "Imie i nazwisko", "Adres zamieszkania";
+    
+SELECT * FROM client_credentials_view;
+
+--
+-- Widok do wyswietlania transakcji
+--
+
+CREATE VIEW transactions_view AS
+SELECT
+    MAX(transaction_type.transaction_name) AS "Rodzaj transakcji",
+    transactions.amount AS "Kwota",
+    accounts.account_number AS "Numer konta"
+FROM 
+    transactions
+JOIN
+    transaction_type ON transactions.type_id = transaction_type.id
+JOIN
+    accounts ON accounts.id = transactions.account_id
+GROUP BY
+    transactions.amount, accounts.account_number;
+    
+--
+-- Widok do wyswietlana wydziałów
+--
+
+CREATE VIEW departments_view AS
+SELECT
+    departments.department_name AS "Oddzial",
+    departments.address AS "Adres",
+    COUNT(employees.id) AS "Liczba pracownikow"
+FROM 
+    departments
+JOIN
+    employees ON departments.id = employees.department_id
+GROUP BY
+    departments.department_name, departments.address;
+    
+SELECT * FROM departments_view;
+
+--
+-- Indeksy
+--
+
+CREATE INDEX client_id_index ON clients(id);
+CREATE INDEX employee_id_index ON employees(id);
+CREATE INDEX card_number_index ON credit_card(card_number);
+CREATE INDEX card_producent_index ON credit_card(producent_name);
+
+CREATE INDEX transaction_id_index ON transactions(id);
+CREATE INDEX transaction_amount_index ON transactions(amount);
+CREATE INDEX transaction_type_index ON transactions(type_id);
+CREATE INDEX transaction_account_index ON transactions(account_id);
+
+CREATE INDEX department_name_index ON departments(department_name);
+CREATE INDEX department_id_index ON departments(id);
+
+CREATE INDEX account_number_index ON accounts(account_number);
+
+--
+-- Usuniecie widokow
+--
+
+DROP VIEW departments_view;
+DROP VIEW transactions_view;
+DROP VIEW client_cards_view;
+DROP VIEW check_balance_view;
+DROP VIEW client_credentials_view;
+
+--
+-- Usuniecie indeksow
+--
+
+DROP INDEX client_id_index ON clients;
+DROP INDEX employee_id_index ON employees;
+DROP INDEX card_number_index ON credit_card;
+DROP INDEX card_producent_index ON credit_card;
+
+DROP INDEX transaction_id ON transactions;
+DROP INDEX transaction_amount ON transactions;
+DROP INDEX transaction_type_index ON transactions;
+DROP INDEX transaction_account ON transactions;
+
+DROP INDEX department_name_index ON departments;
+DROP INDEX department_id_index ON departments;
+
+DROP INDEX account_number_index ON accounts;
