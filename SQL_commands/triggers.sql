1b56ef65d6802a65e70947ec573c19cdc74bd6a5
antektoczynski@gmail.com <antektoczynski@gmail.com>
2023-12-27 22:58:46 +0100

    Trigger for transactions implemented

---
+++ SQL_commands/triggers.sql
@@ -0,0 +1,39 @@
+-- CREATE TRIGGER;
+DELIMITER //
+
+CREATE TRIGGER update_balance
+AFTER INSERT ON transactions FOR EACH ROW
+BEGIN
+    DECLARE transaction_amount DECIMAL(15, 2);
+
+    SET transaction_amount = NEW.amount;
+
+    IF MOD(NEW.type_id, 2) = 0 THEN
+        -- Jeśli ID transakcji jest parzyste, odejmij kwotę
+        UPDATE accounts
+        SET balance = balance - transaction_amount
+        WHERE id = NEW.account_id;
+    ELSE
+        -- W przeciwnym razie dodaj kwotę
+        UPDATE accounts
+        SET balance = balance + transaction_amount
+        WHERE id = NEW.account_id;
+    END IF;
+END //
+
+DELIMITER ;
+
+INSERT INTO transactions (amount, type_id, account_id)
+VALUES
+(11.49, 3, 1);
+
+DROP TRIGGER update_balance; 
+
+SELECT * FROM accounts;
+
+SELECT * FROM transaction_type;
+
+DESC transactions;
+
+
+
